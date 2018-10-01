347bbaf8a0f5641d2f32ec34c9649bc866bd2243
Virginia Dooley <vcdooley@gmail.com>
2018-10-01 16:32:06 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,9 @@
 def key_for_min_value(name_hash)
   lowest_key = nil  
   lowest_value = nil 
-  name_hash.collect do |key, value|
+  name_hash.each do |key, value|
     if lowest_value < value || lowest_value = value
-      lowest_value 
+      lowest_value = 
     end
   end
   lowest_key
